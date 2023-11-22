4d12014264015b825a92738c419f5b97bdd311af
nanorisrael <israel.nanor@compozelabs.com>
2023-11-22 08:54:30 +0000

    Add sample aggregation queries

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Order, OrderItem, Collection, Product
+from django.db.models.aggregates import Count, Sum, Min, Max, Avg
+
 
 # Create your views here.
 def say_hello(request):
-	return HttpResponse('Hello world')
+	# how many orders exist
+	query_set = Order.objects.aggregate(count=Count('id'))
+	# how many of product 1 has been placed
+	result2 = OrderItem.objects.filter(product__id=1).aggregate(quantity_sold=Sum('quantity'))
+	# how many orders belong to customer 1
+	result3 = Order.objects.filter(customer__id=1).aggregate(quantity_placed=Count('id'))\
+
+	result4 = Product.objects.filter(collection__id=3).aggregate(
+		min=Min('unit_price'),
+		max=Max('unit_price'),
+		average=Avg('unit_price')
+	)
+
+	print(query_set)
+	print(result2)
+	print(result3)
+	print(result4)
+	return render(request, 'hello.html')
