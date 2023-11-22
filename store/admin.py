226ce96cf2b72a4e0a3ccecf6d8e1fa970e4656e
nanorisrael <israel.nanor@compozelabs.com>
2023-11-22 17:08:01 +0000

    Add django rest framework

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,10 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ['title', 'unit_price', 'inventory_status']
+	list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
 	list_editable = ['unit_price']
 	list_per_page = 10
+	list_select_related = ['collection']
 
 	@admin.display(ordering=['inventory'])
 	def inventory_status(self, product):
@@ -13,5 +14,8 @@ class ProductAdmin(admin.ModelAdmin):
 			return 'Low'
 		else:
 			return 'Ok'
+	def collection_title(self, product):
+		return product.collection.title
+
 # Register your models here.
 admin.site.register(models.Collection)
