2830fbf2622234c07eb8f1778ff33776281453ac
nanorisrael <israel.nanor@compozelabs.com>
2023-11-22 17:40:52 +0000

    Update store views

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view()
@@ -9,6 +12,11 @@ def product_list(request):
 	return Response('ok');
 
 @api_view()
-def product_detail(request):
-	return Response('ok');
+def product_detail(request, id):
+	try:
+		product = Product.objects.get(pk=id)
+		serializer = ProductSerializer(product)
+		return Response(serializer.data);
+	except Product.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
