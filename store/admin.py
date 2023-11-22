9948d79bf03f1d1351974174b409d7c713b98b99
nanorisrael <israel.nanor@compozelabs.com>
2023-11-22 13:07:45 +0000

    Register collection and product in admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['title', 'unit_price']
+	list_editable = ['unit_price']
+	list_per_page = 10
 # Register your models here.
+admin.site.register(models.Collection)
